a2c8b66770b0b885934283066539fe34ca34875e
Einar <einar.bergviken@gmail.com>
2026-09-02 10:03:30 +0200

    09-01 work

---
+++ 1.1/helloWorld.c
@@ -0,0 +1,33 @@
+/* Nedan följer Einar Bergvikens lösningar för delkapitel 1.1
+*/
+
+#include <stdio.h>
+
+// 1.1.1 
+// a)
+void oneOneOne() {
+    char å = 
+    printf("Låt oss göra lite matte/n");
+}
+
+// b)
+/*
+- Preprocessor är steget som processerar filen och gör saker för att förbereda innan kompilering.
+Saker som t.ex att ta bort kommentarer, fixar #include osv. 
+
+- Kompilatorn är det steget som tar en en .c fil och översätter den till assemblerspråk.
+
+- Länkare
+
+
+*/
+
+// 1.1.2
+
+
+
+void main() {
+    helloWorld();
+    printMathematics();
+
+}
