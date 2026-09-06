882aa14775a9f0ab9663496232b5b5baa3e8fbe4
Einar <einar.bergviken@gmail.com>
2026-09-06 20:10:16 +0200

    1.1.2 & 1.1.3

--- 1.1/oneOne.c
+++ 1.1/oneOne.c
@@ -3,14 +3,16 @@
 #include <stdio.h>
 #include <windows.h>
 
-// 1.1.1 
+// 1.1.1 --------------------------------------------------------------
 // a)
 void oneOneOne() {
     int number1 = 5;
     int number2 = 10;
     int result = number1*number2;
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
     printf("Låt oss göra lite matte\n");
-    printf("%d",&result);
+    printf("%d * %d = %d \n", number1, number2, result);
 }
 
 // b)
@@ -21,18 +23,55 @@ Saker som t.ex att ta bort kommentarer, fixar #include osv.
 - Kompilatorn är det steget som tar en en .c fil och översätter den till assemblerspråk.
 
 - Länkare tar olika filer med binära instruktioner och länkar dem i den ordningen de ska utföras och skapar
-en exekverbar fil
+skapar en exekverbar fil
 */
 
-// 1.1.2
+// 1.1.2 --------------------------------------------------------------
+/*
+a) vad är det hexadecimala värdet för tecknet ”$”?
+b) vilka två heltal utgör gränserna för alla versaler i LATIN-1?
+c) vilken datatyp används i C för att representera ASCII-tecken?
+
+a) 0x24
+b) 65 och 90
+c) char
 
 
+d) skriv ett eget program som skriver ut:
+I C kan vi göra en massa saker med escape-sekvenser:
+vi har tabb
+eller vi kan aktivera ett larm
+(programmet ska larma vid exekvering)
+OBS: tomrummet mellan ”har” och ”tabb” är ett tabulatortecken.
+*/
 
-int main(void) {
+void oneOneTwoD() {
     SetConsoleCP(CP_UTF8);
     SetConsoleOutputCP(CP_UTF8);
-    printf("åäö\n");
+    printf("I C kan vi göra en massa saker med escape-sekvenser:\n");
+    printf("vi har tabb\teller vi kan aktivera ett larm\n");
+    printf("\a"); // Aktiverar ljud vid körning?
+}
 
+// 1.1.3 --------------------------------------------------------------
+
+#if 0
+a) /* printf("Skriv ut..."); */
+b) // printf("Skriv */ ut...");
+c) printf("// Skriv ut...");
+d) printf(//"Skriv ut...");
+e) printf(/* Skriv ut... */""); 
+#endif
+
+// a) Kommer inte skriva ut någonting eftersom raden är kommenterad men komplierar.
+// b) Samma som ovan; kommer inte skriva ut någonting eftersom raden är kommenterad men komplierar.
+// c) Kommer skriva ut // Skriv ut... eftersom det är en korrekt printf-sats.
+// d) Kompilerar inte eftersom // kommenterar bort resten av printf-satsen och printf saknar ");" .
+// e) Kommer skriva ut ingenting eftersom det är en korrekt printf-sats men med en tom sträng.
+
+
+int main(void) { // Körning av funktioner för delkapitel 1.1 :)
     oneOneOne();
+    oneOneTwoD();
     return 0;
 }
